e5328213dcd453d5603ba6ae1e959d8a635514fb
antretrou <169134008+eric-423@users.noreply.github.com>
2025-11-30 00:11:42 +0700

    Add cart item feature with controller, service, and entity
    Introduced CartItem entity, DTO, request payload, repository, service interface and implementation, and controller to support cart item operations for users. Updated security configuration to restrict /api/cart-items/** endpoints to CUSTOMER role. Modified Combo, Product, and Users entities to establish relationships with CartItem.

---
+++ BE/src/main/java/com/capstone/tamtech/capstone/entities/CartItem.java
@@ -0,0 +1,36 @@
+package com.capstone.tamtech.capstone.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cart_items")
+@Entity
+@Data
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartId;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    private String note;
+
+    @ManyToOne(cascade = {jakarta.persistence.CascadeType.PERSIST, jakarta.persistence.CascadeType.MERGE, jakarta.persistence.CascadeType.REFRESH, jakarta.persistence.CascadeType.DETACH})
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(cascade = {jakarta.persistence.CascadeType.PERSIST, jakarta.persistence.CascadeType.MERGE, jakarta.persistence.CascadeType.REFRESH, jakarta.persistence.CascadeType.DETACH})
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @ManyToOne(cascade = {jakarta.persistence.CascadeType.PERSIST, jakarta.persistence.CascadeType.MERGE, jakarta.persistence.CascadeType.REFRESH, jakarta.persistence.CascadeType.DETACH})
+    @JoinColumn(name = "combo_id")
+    private Combo combo;
+}
