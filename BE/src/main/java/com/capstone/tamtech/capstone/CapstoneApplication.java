c596374c895b077867755f567f765ab9606a4bc6
antretrou <169134008+eric-423@users.noreply.github.com>
2025-12-04 02:12:59 +0700

    Set default timezone to Asia/Ho_Chi_Minh
    Configured the application to use Asia/Ho_Chi_Minh as the default timezone by setting it in the main method. This ensures consistent timezone handling throughout the backend.

--- BE/src/main/java/com/capstone/tamtech/capstone/CapstoneApplication.java
+++ BE/src/main/java/com/capstone/tamtech/capstone/CapstoneApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class CapstoneApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(CapstoneApplication.class, args);
 	}
 
