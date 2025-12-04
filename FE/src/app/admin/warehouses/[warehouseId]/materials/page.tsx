b13d244a490b295a5da92c80d3dff555e958850c
lequanghuyyyy <133076256+lequanghuyyyy@users.noreply.github.com>
2025-12-04 22:04:55 +0700

    code build sc

--- FE/src/app/admin/warehouses/[warehouseId]/materials/page.tsx
+++ FE/src/app/admin/warehouses/[warehouseId]/materials/page.tsx
@@ -37,18 +37,18 @@ export default function WarehouseMaterialsPage({ params }: WarehouseMaterialsPag
     const [searchKeyword, setSearchKeyword] = useState('');
     const [typeFilter, setTypeFilter] = useState('');
 
-    // Hide main page scrollbar when any dialog is open
-    useEffect(() => {
-        const isAnyDialogOpen = showAddDialog || showEditDialog || showImportDialog;
-        if (isAnyDialogOpen) {
-            document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = '';
-        }
-        return () => {
-            document.body.style.overflow = '';
-        };
-    }, [showAddDialog, showEditDialog, showImportDialog]);
+    // // Hide main page scrollbar when any dialog is open
+    // useEffect(() => {
+    //     const isAnyDialogOpen = showAddDialog || showEditDialog || showImportDialog;
+    //     if (isAnyDialogOpen) {
+    //         document.body.style.overflow = 'hidden';
+    //     } else {
+    //         document.body.style.overflow = '';
+    //     }
+    //     return () => {
+    //         document.body.style.overflow = '';
+    //     };
+    // }, [showAddDialog, showEditDialog, showImportDialog]);
 
     useEffect(() => {
         params.then(p => {
